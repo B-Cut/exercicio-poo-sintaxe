18e977a042b4083ac5090ca10c4e85952c7e555a
B-Cut <caelgonsalvesrj@gmail.com>
2022-08-25 13:51:46 -0300

    Exercicio 3 feito

---
+++ q3.java
@@ -0,0 +1,12 @@
+class q3{
+    public static void main(String[] args){
+            
+            int a = 1;
+            int b = 999;
+
+            //Soma de todos os termos entre dois números
+            int soma = (b-a+1)*(a+b)/2;
+
+            System.out.println("A soma de todos os números entre "+ a +" e "+ b+ " é " + soma);
+    }
+}
